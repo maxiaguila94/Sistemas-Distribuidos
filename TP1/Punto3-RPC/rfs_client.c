cf49eee0d15b009a2f861ad3b3814d1042b32882
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2018-08-23 13:24:19 -0300

    cliente arreglado falta menu y write

--- TP1/Punto3-RPC/rfs_client.c
+++ TP1/Punto3-RPC/rfs_client.c
@@ -5,20 +5,24 @@
  */
 
 #include "rfs.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 void
-rfs_1(char *host)
+rfs_1(char *host, char *file_name)
 {
+
 	CLIENT *clnt;
 	int  *result_1;
 	open_record  rfs_open_1_arg;
 	file_data  *result_2;
 	read_record  rfs_read_1_arg;
-	file_data  *result_3;
+	int  *result_3;
 	write_record  rfs_write_1_arg;
 	int  *result_4;
 	int  rfs_close_1_arg;
+	int fd, n;
 
 #ifndef	DEBUG
 	clnt = clnt_create (host, RFS, RFS_VERS_1, "udp");
@@ -28,22 +32,43 @@ rfs_1(char *host)
 	}
 #endif	/* DEBUG */
 
+	// RFS OPEN
+	rfs_open_1_arg.file_name = file_name; /* define archivo a leer */
+	rfs_open_1_arg.flags = O_RDWR; /* define permisos */
 	result_1 = rfs_open_1(&rfs_open_1_arg, clnt);
 	if (result_1 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
+		clnt_perror (clnt, "Fallo llamada open");
 	}
-	result_2 = rfs_read_1(&rfs_read_1_arg, clnt);
-	if (result_2 == (file_data *) NULL) {
-		clnt_perror (clnt, "call failed");
+	fd = *result_1;
+	if (fd == -1) {
+		printf("Error al abrir el archivo\n"); return;
 	}
+	rfs_read_1_arg.fd = fd;
+	rfs_read_1_arg.count = 20;
+
+	// RFS READ
+	do {
+		result_2 = rfs_read_1(&rfs_read_1_arg, clnt);
+		if (result_2 == (file_data *) NULL) {
+			clnt_perror (clnt, "Fallo llamada read");
+		}
+		for (n=0; n < result_2->file_data_len; ++n)
+			putchar(result_2->file_data_val[n]);
+	} while (result_2->file_data_len == 20);
+	putchar('\n');
+
+
 	result_3 = rfs_write_1(&rfs_write_1_arg, clnt);
 	if (result_3 == (file_data *) NULL) {
 		clnt_perror (clnt, "call failed");
 	}
+
+	
 	result_4 = rfs_close_1(&rfs_close_1_arg, clnt);
 	if (result_4 == (int *) NULL) {
 		clnt_perror (clnt, "call failed");
 	}
+
 #ifndef	DEBUG
 	clnt_destroy (clnt);
 #endif	 /* DEBUG */
@@ -54,12 +79,15 @@ int
 main (int argc, char *argv[])
 {
 	char *host;
-
-	if (argc < 2) {
-		printf ("usage: %s server_host\n", argv[0]);
+	char *file_name;
+	/* Se deben pasar nombre de host y de archivo => argc=3 */
+	if (argc < 3) {
+		printf ("usage: %s server_host filename\n", argv[0]);
 		exit (1);
 	}
 	host = argv[1];
-	rfs_1 (host);
-exit (0);
+	/* nombre del host remoto */
+	file_name = argv[2]; /* nombre del archivo a leer */
+	rfs_1 (host, file_name);
+	exit (0);
 }
--- TP1/Punto3-RPC/rfs_server.c
+++ TP1/Punto3-RPC/rfs_server.c
@@ -9,24 +9,23 @@
 int *
 rfs_open_1_svc(open_record *argp, struct svc_req *rqstp)
 {
-	static int  result;
-
-	/*
-	 * insert server code here
-	 */
-
+	static int result;
+	printf("llamada open\n");
+	result = open(argp->file_name, argp->flags);
 	return &result;
 }
 
 file_data *
 rfs_read_1_svc(read_record *argp, struct svc_req *rqstp)
 {
-	static file_data  result;
-
-	/*
-	 * insert server code here
-	 */
-
+	static file_data result;
+	int n;
+	printf("llamada read\n");
+	result.file_data_val = (char *) malloc(argp->count);
+	if (result.file_data_val == 0)
+		result.file_data_len = 0;
+	else
+		result.file_data_len = read(argp->fd, result.file_data_val, argp->count);
 	return &result;
 }
 
@@ -45,11 +44,8 @@ rfs_write_1_svc(write_record *argp, struct svc_req *rqstp)
 int *
 rfs_close_1_svc(int *argp, struct svc_req *rqstp)
 {
-	static int  result;
-
-	/*
-	 * insert server code here
-	 */
-
+	static int result;
+	printf("llamada close\n");
+	result = close(*argp);
 	return &result;
 }
