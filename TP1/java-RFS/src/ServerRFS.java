28469f644db4d1c994e88966f05400499e0d6a49
Maximiliano Aguila <maxiaguila@gmail.com>
2018-09-02 17:14:18 +0000

    informe y login

---
+++ TP1/java-RFS/src/ServerRFS.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ServerRFS {
+
+	
+	public ServerRFS() {
+		
+	}
+		
+    public String read( String ruta ){
+        FileReader fr = null;
+        BufferedReader br = null;
+        //Cadena de texto donde se guardara el contenido del archivo
+        String contenido = "";
+        try
+        {
+            //ruta puede ser de tipo String o tipo File
+            fr = new FileReader( ruta );
+            br = new BufferedReader( fr );
+ 
+            String linea;
+            //Obtenemos el contenido del archivo linea por linea
+            while( ( linea = br.readLine() ) != null ){ 
+                contenido += linea + "\n";
+            }
+ 
+        }catch( Exception e ){ 
+        	e.printStackTrace();
+        }
+        finally
+        {
+            try{
+                br.close();
+            }catch( Exception ex ){}
+        }
+        return contenido;
+    }
+	
+	
+	
+	
+	
+	
+	
+	public void write (String file_name, String data) {
+		
+	}
+	
+}
